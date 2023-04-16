c0a07e92ee5c4bf488266a0fd1c437e153f922a7
user <user@192.168.1.136>
2023-04-16 18:46:13 +0100

    PDA

---
+++ part_2_code/card_game_testing.py
@@ -0,0 +1,24 @@
+class CardGame:
+
+
+  def check_for_ace(self, card):
+    if card.value = 1:
+      return True
+    # else - Missing semicolon
+      return False
+   
+
+#   dif highest_card(self, card1 card2): - dif should be def, also missing a comma between card 1 and 2
+#   if card1.value > card2.value: - error due to card1 and card2 being undefined
+    return card
+  else:
+    return card2
+  
+
+
+def cards_total(self, cards):
+#   total - total is not initialized
+  for card in cards:
+    total += card.value
+    # return "You have a total of" + total - return method should be outside of the for loop
+    # also should be You have a total of " the space is missing
